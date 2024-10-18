1dbae38e77633847c389b7a5d050510dae529886
Jay <122465785+GoblinJay@users.noreply.github.com>
2024-10-18 15:04:51 -0700

    Added functions is_terminal and get_scores

--- homework_2_code_files/GameStatus_5120.py
+++ homework_2_code_files/GameStatus_5120.py
@@ -18,7 +18,20 @@ class GameStatus:
         YOUR CODE HERE TO CHECK IF ANY CELL IS EMPTY WITH THE VALUE 0. IF THERE IS NO EMPTY
         THEN YOU SHOULD ALSO RETURN THE WINNER OF THE GAME BY CHECKING THE SCORES FOR EACH PLAYER 
         """
-		
+		#Check if there are any empty cells (value 0)
+		for row in self.board_state:
+			for cell in row:
+				if cell == 0:
+					return False #Game isn't over yet since theres still empty cells
+		#If there are no empty cells then we check to see if theres a winner by calculating scores
+		score = self.get_scores(True)
+		if score > 0:
+			self.winner = "Human" #The player wins
+		elif score < 0:
+			self.winner = "AI" #The AI wins
+		else:
+			self.winner = "Draw"
+		return True #Returns true to signal the end of the game	
 
 	def get_scores(self, terminal):
 		"""
@@ -33,7 +46,44 @@ class GameStatus:
 		cols = len(self.board_state[0])
 		scores = 0
 		check_point = 3 if terminal else 2
-		
+
+		#Check horizontal
+		for row in self.board_state:
+			for i in range(cols - check_point + 1): #Checks for triplets/pairs in each row
+				triplet = row[i:i+check_point]
+				if all(x == 1 for x in triplet):
+					scores += 1 #Human player
+				elif all(x == -1 for x in triplet):
+					scores -= 1 #AI player
+
+		#Check vertical
+		for col in range(cols):
+			for row in range(rows - check_point + 1): #Checks for triplets/pairs in each column
+				triplet = [self.board_state[row+i][col] for i in range(check_point)]
+				if all(x == 1 for x in triplet):
+					scores += 1 #Human player
+				elif all(x == -1 for x in triplet):
+					scores -= 1 #AI player
+
+		#Check diagonal
+		for row in range(rows - check_point + 1):
+			for col in range(cols - check_point + 1):
+				#From top left to bottom right
+				triplet = [self.board_state[row+i][col+i] for i in range(check_point)]
+				if all(x == 1 for x in triplet):
+					scores += 1 #Human player
+				elif all(x == -1 for x in triplet):
+					scores -= 1 #AI player
+
+				#From top right to bottom left
+				triplet = [self.board_state[row+i][col+check_point-i-1] for i in range(check_point)]
+				if all(x == 1 for x in triplet):
+					scores += 1 #Human player
+				elif all(x == -1 for x in triplet):
+					scores -= 1 #AI player
+
+		return scores
+
 	    
 
 	def get_negamax_scores(self, terminal):
